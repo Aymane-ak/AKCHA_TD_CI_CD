a81d9bb43f1212403a359b113aebd4f7a278d90e
aymane-ak <aymaneak30@gmail.com>
2025-09-19 00:59:54 +0200

    fix : run server on 0.0.0.0:5000 instead of debug mode

--- TP Docker/app.py
+++ TP Docker/app.py
@@ -8,5 +8,4 @@ def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    # Lancement du serveur en mode debug
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
